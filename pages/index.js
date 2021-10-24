c5294ca09e509f443362258720aa5b86fdd52289
Edriss563 <edrissalokozay77@gmail.com>
2021-10-24 10:25:24 -0700

    another change

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ function Home() {
          <section id="banner-area">
             <div className="banner">
                <h1>Books made the world!</h1>
-               <p>Search through hundreds of books <br /> change your life by reading.</p>
+               <p>Search through hundreds of books <br /> and change your life by reading.</p>
                <div className="search-bar">
                   <input type="text" name="search" className="search" placeholder="search for books.." />
                   <button className="btn-search">Search</button>
